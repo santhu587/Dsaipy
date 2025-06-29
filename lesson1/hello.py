ab9dc2f06909cacecce37dde3011fb3aca14397b
santhu587 <santhoshckm2001@gmail.com>
2025-06-29 23:11:35 +0530

    pyhton ls1 ls2

---
+++ lesson1/hello.py
@@ -0,0 +1,27 @@
+print("hello world")
+
+line01 = "********************"  # header / footer
+line02 = "*                  *"  # re-use
+line03 = "*     WELCOME!     *"
+
+
+# starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
+
+print("santhu") if 10>5 else print("sandii")
